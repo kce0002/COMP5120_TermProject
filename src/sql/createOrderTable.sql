7562201d35edcbd6aeaf7930032ec3273f1101cd
Kyle Ehlers <kce0002@auburn.edu>
2018-09-28 01:02:55 -0500

    Hopefully fixed order error

--- src/sql/createOrderTable.sql
+++ src/sql/createOrderTable.sql
@@ -1,16 +1,16 @@
 CREATE TABLE Order (OrderID INT, CustomerID INT, EmployeeID INT, OrderDate DATE, ShippedDate DATE, ShipperID INT);
 
 INSERT INTO Order
-VALUES (1, 1, 1, STR_TO_DATE('08/01/2016', '%m/%d/%Y'), STR_TO_DATE('08/03/2016', '%m/%d/%Y'), 1);
+VALUES (1, 1, 1, '2016-08-01', '2016-08-03', 1);
 INSERT INTO Order
-VALUES (2, 1, 2, STR_TO_DATE('08/04/2016', '%m/%d/%Y'), NULL, NULL);
+VALUES (2, 1, 2, '2016-08-04', NULL, NULL);
 INSERT INTO Order
-VALUES (3, 2, 1, STR_TO_DATE('08/01/2016', '%m/%d/%Y'), STR_TO_DATE('08/04/2016', '%m/%d/%Y'), 2);
+VALUES (3, 2, 1, '2016-08-01', '2016-08-04', 2);
 INSERT INTO Order
-VALUES (4, 4, 2, STR_TO_DATE('08/04/2016', '%m/%d/%Y'), STR_TO_DATE('08/04/2016', '%m/%d/%Y'), 1);
+VALUES (4, 4, 2, '2016-08-04', '2016-08-04', 1);
 INSERT INTO Order
-VALUES (5, 1, 1, STR_TO_DATE('08/04/2016', '%m/%d/%Y'), STR_TO_DATE('08/05/2016', '%m/%d/%Y'), 1);
+VALUES (5, 1, 1, '2016-08-04', '2016-08-05', 1);
 INSERT INTO Order
-VALUES (6, 4, 2, STR_TO_DATE('08/04/2016', '%m/%d/%Y'), STR_TO_DATE('08/05/2016', '%m/%d/%Y'), 1);
+VALUES (6, 4, 2, '2016-08-04', '2016-08-05', 1);
 INSERT INTO Order
-VALUES (7, 3, 1, STR_TO_DATE('08/04/2016', '%m/%d/%Y'), STR_TO_DATE('08/04/2016', '%m/%d/%Y'), 1);
+VALUES (7, 3, 1, '2016-08-04', '2016-08-04', 1);
