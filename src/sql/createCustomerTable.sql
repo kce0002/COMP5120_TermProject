1b64baff9a5d78be2d456a43848dc3c14033c15e
Kyle Ehlers <kce0002@auburn.edu>
2018-09-28 00:16:36 -0500

    Creating sql subdir

---
+++ src/sql/createCustomerTable.sql
@@ -0,0 +1,10 @@
+CREATE TABLE Customer (CustomerID INT, LastName VARCHAR(20), Firstname VARCHAR(20), Phone VARCHAR(20));
+
+INSERT INTO Customer
+VALUES (1, 'lastname1', 'firstname1', '334-001-001');
+INSERT INTO Customer
+VALUES (2, 'lastname2', 'firstname2', '334-002-002');
+INSERT INTO Customer
+VALUES (3, 'lastname3', 'firstname3', '334-003-003');
+INSERT INTO Customer
+VALUES (4, 'lastname4', 'firstname4', '334-004-004');
