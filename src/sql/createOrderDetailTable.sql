1541f226c3f5f57dd322416809a3599fe9dc9f28
Kyle Ehlers <kce0002@auburn.edu>
2018-09-28 00:53:09 -0500

    Fixed order detail error

--- src/sql/createOrderDetailTable.sql
+++ src/sql/createOrderDetailTable.sql
@@ -1,24 +1,24 @@
 CREATE TABLE OrderDetail (BookID INT, OrderID INT, Quantity INT);
 
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (1, 1, 2);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (4, 1, 1);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (6, 2, 2);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (7, 2, 3);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (5, 3, 1);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (3, 4, 2);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (4, 4, 1);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (7, 4, 1);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (1, 5, 1);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (1, 6, 2);
-INSERT INTO Orderdetail
+INSERT INTO OrderDetail
 VALUES (1, 7, 1);
