ad1e5a2a92d44dcc8fcdb302c2d59571bb60ee91
Kyle Ehlers <kce0002@auburn.edu>
2018-09-28 00:06:26 -0500

    Added first create table sql file

---
+++ src/createBookTable.sql
@@ -0,0 +1,18 @@
+CREATE TABLE Book (BookID INT, Title VARCHAR(20), UnitPrice DECIMAL(5, 2), Author VARCHAR(20), Quantity INT, SupplierID INT, SubjectID INT);
+
+INSERT INTO Book
+VALUES (1, 'book1', 12.34, 'author1', 5, 3, 1);
+INSERT INTO Book
+VALUES (2, 'book2', 56.78, 'author2', 2, 3, 1);
+INSERT INTO Book
+VALUES (3, 'book3', 90.12, 'author3', 10, 2, 1);
+INSERT INTO Book
+VALUES (4, 'book4', 34.56, 'author4', 12, 3, 2);
+INSERT INTO Book
+VALUES (5, 'book5', 78.90, 'author5', 5, 2, 2);
+INSERT INTO Book
+VALUES (6, 'book6', 12.34, 'author6', 30, 1, 3);
+INSERT INTO Book
+VALUES (7, 'book7', 56.90, 'author2', 17, 3, 4);
+INSERT INTO Book
+VALUES (8, 'book8', 33.44, 'author7', 2, 1, 3);
